9cdcea4cbdc2ce252b5d14a45e3dc790d6a7bff9
Bud Linville <a0l035t@HOMEOFFICE.wal-mart.com>
2019-10-14 14:47:13 -0500

    first commit

---
+++ src/data/socialProfiles.js
@@ -0,0 +1,33 @@
+import emailIcon from '../assets/email_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+import twitterIcon from '../assets/twitter_icon.png';
+
+const SOCIAL_PROFILES = [
+    {
+        id: 1,
+        title: 'Email',
+        link: 'mailto:budlinville@gmail.com',
+        image: emailIcon
+    },
+    {
+        id: 2,
+        title: 'Github',
+        link: 'https://github.com/budlinville',
+        image: githubIcon
+    },
+    {
+        id: 3,
+        title: 'Linkedin',
+        link: 'https://www.linkedin.com/in/bud-linville/',
+        image: linkedinIcon
+    },
+    {
+        id: 4,
+        title: 'Twitter',
+        link: 'https://twitter.com/budlinville',
+        image: twitterIcon
+    }
+];
+
+export default SOCIAL_PROFILES;
